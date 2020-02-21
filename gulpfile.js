8d240a0cdd167ef01cc375eb977f7e690fd16d39
Cassidy <schnauzersal@gmail.com>
2020-02-20 17:18:31 -0700

    Added some basic styling and started 404

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 function style() {
  return gulp.src('./scss/**/*.scss')
- .pipe(sass())
+ .pipe(sass({outputStyle: 'nested'}))
  .pipe(gulp.dest('./css'))
  .pipe(browserSync.stream());
 }
@@ -13,7 +13,7 @@ function watch() {
  baseDir: './'
  }
  });
- gulp.watch('./scss/**/*.scss', style);
+ gulp.watch('./scss/**/*.scss', {ignoreInitial: false}, style);
  gulp.watch('./*.html').on('change', browserSync.reload);
  gulp.watch('./js/**/*.js').on('change', browserSync.reload);
 }
